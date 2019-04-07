06e62fca8c7821e221104731c591d5b27c1b552d
GoodSix <1434389213@qq.com>
2019-04-07 19:31:50 +0800

    empty

--- ubuntu_helper/安装nodejs_npm/index.sh
+++ ubuntu_helper/安装nodejs_npm/index.sh
@@ -7,3 +7,5 @@ unar nodejs.tar.xz;
 mv node-v10.15.3-linux-x64 /usr/local/nodejs;
 
 cat "export PATH=\"\$PATH:/usr/local/nodejs/bin\""; >> /etc/profile;
+source /etc/profile;
+echo "安装完毕，但是你需要重启才会在所有地方生效";
