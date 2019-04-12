16b981a821a7e707ac0cdfcb336ea53f58b950fa
GoodSix <43713756+GoodSix@users.noreply.github.com>
2019-04-12 13:58:13 +0800

    Update index.sh

--- helper/lnmp_Nginx_Mysql_PHP环境搭建/index.sh
+++ helper/lnmp_Nginx_Mysql_PHP环境搭建/index.sh
@@ -4,5 +4,6 @@ wget -O lnmp.gz http://soft.vpser.net/lnmp/lnmp1.5-full.tar.gz;
 if [[ -f 'lnmp.gz' ]]; then
     tar -zxvf lnmp.gz;
     chmod +x lnmp1.5-full;
-    lnmp1.5-full/install.sh
+    cd lnmp1.5-full;
+    ./install.sh
 fi;
