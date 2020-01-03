3ae3ab47e0e0a9cd5323770bd5f36113fda0b524
eoogo <1434389213@qq.com>
2020-01-03 10:26:15 +0800

    写错了.

--- ubuntu/php-install-5.6-xdebug.sh
+++ ubuntu/php-install-5.6-xdebug.sh
@@ -24,7 +24,7 @@ xdebug.remote_autostart=On
 xdebug.remote_enable=On
 xdebug.remote_host=0.0.0.0
 xdebug.remote_port=9001' >> /etc/php/5.6/cli/php.ini && \
-  service php7.2-fpm restart # 重启PHP服务
+  service php5.6-fpm restart # 重启PHP服务
 }
 
 # 开启php5.6的xdebug扩展
