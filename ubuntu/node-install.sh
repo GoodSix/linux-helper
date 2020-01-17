9deabd36cb28214c8679f2cae7d1f0c0f6d6f762
eoogo <1434389213@qq.com>
2020-01-17 22:30:14 +0800

    node

--- ubuntu/node-install.sh
+++ ubuntu/node-install.sh
@@ -8,11 +8,14 @@ before() {
 }
 
 setup() {
-    wget https://nodejs.org/dist/v12.14.0/node-v12.14.0-linux-x64.tar.xz -O nodejs.tar.xz
-    mkdir nodejs && tar -xvf nodejs.tar.xz -C nodejs
-    mv nodejs/`ls nodejs/` /usr/local/nodejs
-    rm -rf nodejs.tar.xz nodejs
-    echo 'PATH="$PATH:/usr/local/nodejs/bin"' >> ~/.bashrc
+    # wget https://nodejs.org/dist/v12.14.0/node-v12.14.0-linux-x64.tar.xz -O nodejs.tar.xz
+    # mkdir nodejs && tar -xvf nodejs.tar.xz -C nodejs
+    # mv nodejs/`ls nodejs/` /usr/local/nodejs
+    # rm -rf nodejs.tar.xz nodejs
+    # echo 'PATH="$PATH:/usr/local/nodejs/bin"' >> ~/.bashrc
+    apt install nodejs npm -y;
+    npm i -g n;
+    n stable;
 }
 
 # 为当前用户安装nodejs
