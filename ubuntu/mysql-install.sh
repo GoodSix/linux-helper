f785047de405b8a1a6d350d48c61655d761a83a3
eoogo <1434389213@qq.com>
2020-02-18 23:53:01 +0800

    mysql user

--- ubuntu/mysql-install.sh
+++ ubuntu/mysql-install.sh
@@ -8,10 +8,10 @@ setup() {
     if [[ `apt install -y mysql-server-5.7 && service mysql start` ]]; then
         user=`cat /etc/mysql/debian.cnf | grep 'user' | grep -o '= .*$' | head -1`
         password=`cat /etc/mysql/debian.cnf | grep 'password' | grep -o '= .*$' | head -1`
-        mysql -u${user:2} -p${password:2} -e 'grant all privileges on *.* to root@"localhost" identified by "root" with grant option;'
+        mysql -u${user:2} -p${password:2} -e 'grant all privileges on *.* to root@"localhost" identified with mysql_native_password by "root" with grant option;'
         mysql -u${user:2} -p${password:2} -e 'flush privileges;'
         if [[ $eoogo_docker_devt ]]; then # 在容器中自动添加远程连接身份
-            mysql -u${user:2} -p${password:2} -e 'grant all privileges on *.* to root@"%" identified by "root" with grant option;'
+            mysql -u${user:2} -p${password:2} -e 'grant all privileges on *.* to root@"%" identified with mysql_native_password by "root" with grant option;'
         fi
         service mysql stop
         if [[ $eoogo_docker_devt ]]; then # 在容器中引入这个配置默认配置
